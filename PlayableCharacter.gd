3641a0519cf98d3c457e9e2dff723c5acc3534bf
rasmus.karschny <karschny@safelog.de>
2020-12-19 20:22:21 +0100

    player attack

--- PlayableCharacter.gd
+++ PlayableCharacter.gd
@@ -2,5 +2,8 @@ extends Character
 
 func _input(event):
 	if(event.is_action_pressed("attack") && attack_cooldown.is_stopped()):
-		print("attack")
-		attack_cooldown.start()
+		attack()
+
+func attack():
+	attack_cooldown.start()
+	print('attack')
