61c9b294ae57349d47f69e52b94e4276fddaf735
Woodshape <rkarschny@gmail.com>
2020-12-21 20:02:43 +0100

    added invulnerability

--- characters/Character.gd
+++ characters/Character.gd
@@ -6,7 +6,10 @@ const PIXEL_UNITS = 24
 export var movespeed_units := 8.0
 export var max_health := 10.0
 
+onready var damage_animator = $DamageAnimator
 onready var attack_cooldown = $Timers/AttackCooldown
+onready var hitbox = $Hitbox
+
 onready var health = max_health setget set_health
 
 var velocity := Vector2()
@@ -14,17 +17,19 @@ var move_input := Vector2()
 
 
 func _on_Hitbox_damaged(amount, knockback_strength, damage_source, attacker):
-	var temp = health - amount
-	var animation = $DamageAnimator
-	animation.play("damage")
-	set_health(temp)
+	self.health -= amount
+	damage_animator.play("damage")
+	if hitbox.immunity_duration != 0:
+		damage_animator.queue("invulnerability")
 
 func set_health(value):
-	health = value
+	health = clamp(value, 0, max_health)
 	if health <= 0:
 		die()
-	elif health > max_health:
-		health = max_health
 
 func die():
 	queue_free()
+
+
+func _on_Hitbox_immunity_ended():
+	damage_animator.play("reset")
